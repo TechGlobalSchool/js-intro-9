739c6fd20b3f4fe26053fbf70b417125f9fe1999
TechGlobalSchool <tgschool01@gmail.com>
2024-01-27 14:10:32 -0600

    added data types

--- js-basics/05-var-let-const.js
+++ js-basics/05-var-let-const.js
@@ -18,3 +18,9 @@ pi = 5; // TypeError: Assignment to constant variable.
 
 let let_ = 'smth';
 
+let companyName = 'Apple';
+
+let company2Name = 'Microsoft'; 
+
+console.log(companyName); // Apple
+console.log(company2Name); // Microsoft
