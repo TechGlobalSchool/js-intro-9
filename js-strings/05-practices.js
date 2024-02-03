5350c9c3f82d9eabec43f139e6ffe4336145ddb8
TechGlobalSchool <tgschool01@gmail.com>
2024-02-03 14:05:45 -0600

    added more string methods

---
+++ js-strings/05-practices.js
@@ -0,0 +1,11 @@
+let p1 = 'Hello'.toUpperCase().concat(' World').includes('o World');
+
+console.log(p1); // false
+
+
+let p2 = Math.pow(Math.abs(5 - 5), 3);
+
+console.log(p2);
+
+
+let p3 = 'TechGlobal'.slice(3, 7).toUpperCase().includes('hG', 3);
