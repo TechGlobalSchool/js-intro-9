aa89fd68d09b56afd51aad11b3eecbe78c288ba6
TechGlobalSchool <tgschool01@gmail.com>
2024-02-04 13:59:45 -0600

    added more string methods

--- js-strings/05-practices.js
+++ js-strings/05-practices.js
@@ -5,7 +5,13 @@ console.log(p1); // false
 
 let p2 = Math.pow(Math.abs(5 - 5), 3);
 
-console.log(p2);
+console.log(p2); // 0
 
 
 let p3 = 'TechGlobal'.slice(3, 7).toUpperCase().includes('hG', 3);
+
+console.log(p3); // false
+
+let p4 = 'JavaScript'.startsWith('Java').endsWith('Script'); // Error
+
+let p5 = 'Apple'.replaceAll('p', '$').indexOf('$$') >= 0;
