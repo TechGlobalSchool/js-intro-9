f2b9dd3526146c9a6a2df29d8d7b7147117e556e
TechGlobalSchool <tgschool01@gmail.com>
2024-03-18 18:56:44 -0500

    added Product constructor

--- js-oop/02-constructor.js
+++ js-oop/02-constructor.js
@@ -24,4 +24,36 @@ const teacher3 = new Teacher('Johnny', 'B', 45, 'Chemical Engineer');
 
 // Store all the ages in an array -> [47, 45, 45]
 // Store all the first names in an array -> [ 'Alex', 'Bobby', 'Johnny' ]
-// Find and print ythe information of the oldest teacher -> Alex is oldest with the age 47.
+// Find and print the information of the oldest teacher -> Alex is oldest with the age 47.
+
+// const teachers = new Array();
+// teachers.push(teacher1);
+// teachers.push(teacher2);
+// teachers.push(teacher3);
+
+const teachers = [ teacher1, teacher2, teacher3 ];
+
+const ages = teachers.map(teacher => teacher.age);
+
+console.log(ages); // [47, 45, 45]
+
+const names = teachers.map(teacher => teacher.firstname);
+
+console.log(names);
+
+
+// let oldest = teachers[0];
+
+// for(const teacher of teachers) {
+//     if(teacher.age > oldest.age) oldest = teacher;
+// }
+
+const oldest = teachers.reduce((acc, curr) => {
+    return curr.age > acc.age ? curr : acc;
+    // if(curr.age > acc.age) return curr;
+    // return acc;
+});
+
+
+
+console.log(`${oldest.firstname} is oldest with the age ${oldest.age}.`); // Alex is oldest with the age 47.
