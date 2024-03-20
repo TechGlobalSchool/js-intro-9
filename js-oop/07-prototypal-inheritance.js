84e9996c7ab9815a70e8bab448b6cfdeb548e14d
TechGlobalSchool <tgschool01@gmail.com>
2024-03-20 13:15:57 -0500

    added inheritance

--- js-oop/07-prototypal-inheritance.js
+++ js-oop/07-prototypal-inheritance.js
@@ -29,3 +29,27 @@ const programmer_mustafa = new Programmer();
 programmer_mustafa.code();
 programmer_mustafa.eat();
 programmer_mustafa.sleep();
+
+
+// Create Singer template which inherits from Person and add sing method to it.
+// Create a Singer object and execute the sing(), eat() and sleep() method
+
+// PROTOTYPE SINGER 
+// constructor
+function Singer() {
+
+}
+
+// Inheriting all the Person Prototype functions into Singer prototype
+Singer.prototype = Object.create(Person.prototype);
+
+// Creating a new Singer Prototype function
+Singer.prototype.sing = function () {
+    console.log('SING');
+}
+
+const singer1 = new Singer();
+
+singer1.eat();
+singer1.sleep();
+singer1.sing();
