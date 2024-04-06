6a4bd02c6fac40e8d2f44d62f6cb30ab1bed5b0c
TechGlobalSchool <tgschool01@gmail.com>
2024-04-06 12:07:36 -0500

    dom 2

---
+++ dom/activity.js
@@ -0,0 +1,16 @@
+/*
+1. Ask user a name => prompt()
+
+    1.1 Alert users name => alert()
+    1.2 Console 'You didn't provide your name. Bye Bye' and quit.
+
+
+2. Confirm if user wants to continie => confirm()
+    2.1 If user says 'YES' => Alert 'You have chosen to continue"
+    2.2 If user says 'NO' => Alert 'You canceled the action'
+
+3. Console users browser innerWidth and innerHeight
+
+4. Reload the page after 10 seconds to start again
+    setTimeout, window.location.reload()
+*/
