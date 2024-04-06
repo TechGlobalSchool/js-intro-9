6740b6aa206fe10ce8b64717e90a0e31d3e9883a
TechGlobalSchool <tgschool01@gmail.com>
2024-04-06 12:50:09 -0500

    activity 1

--- dom/activity.js
+++ dom/activity.js
@@ -14,3 +14,28 @@
 4. Reload the page after 10 seconds to start again
     setTimeout, window.location.reload()
 */
+
+const userName = prompt('What is your name?');
+
+// 1
+if(userName) {
+    // all the work
+    alert(`Welcome ${userName}`);
+
+    // 2
+    const doesUserWantsToContinue = confirm('Do you still want to continue?');
+    if(doesUserWantsToContinue) {
+        alert('You chose to continue')
+    } else {
+        alert('You chose not to continue')
+    }
+
+    // 3
+    console.log(`You window dimesnions height: ${innerHeight} and width: ${innerWidth}`);
+
+    // 4
+    console.log('Page will be reloading in 5 seconds ...');
+    setTimeout(() => location.reload(), 5000)
+} else {
+    console.log("Bye Bye! Name didn't provide")
+}
