4226a7c30c6d81c3943047ee53a5757ae0931fb6
TechGlobalSchool <tgschool01@gmail.com>
2024-04-07 13:15:30 -0500

    acitivty

--- dom/activity/activity.js
+++ dom/activity/activity.js
@@ -5,11 +5,24 @@
 // Step 1
 // Get elements
 // input, btn, result
+const heightEl = document.getElementById('height');
+const weightEl = document.getElementById('weight');
+const btnEl = document.querySelector('button');
+const resultsEl = document.querySelector('#results');
 
-// Step 2
 // Attach an event
-// Get values
-// Do calculations
+btnEl.addEventListener('click', (e) => {
+    e.preventDefault();
+    const heightValue = parseFloat(heightEl.value);
+    const weightValue = parseFloat(weightEl.value);
 
-// Step 3
-// Show result => append to DOM
+    if(heightValue === '' || heightValue === undefined || isNaN(heightValue) || heightValue < 0) {
+        resultsEl.innerHTML = 'Please provide height value'
+    }else if(weightValue === '' || weightValue === undefined || isNaN(weightValue) || weightValue < 0) {
+        resultsEl.innerHTML = 'Please provide weight value'
+    }
+     else {
+        const bmi=  weightValue / ((heightValue / 100) ** 2);
+        resultsEl.innerHTML = bmi;
+    }
+})
